f719d7ff365b0c38fc7de1bdf4f3629d303bb570
SaulEnriqueX <saulmallqui1999@gmail.com>
2023-01-08 00:20:53 -0500

    Se sube el proyecto

---
+++ ProyectoSumaVariables/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblFecha = New System.Windows.Forms.Label()
+        Me.lblMensaje = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtuno = New System.Windows.Forms.TextBox()
+        Me.txtdos = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblFecha
+        '
+        Me.lblFecha.AutoSize = True
+        Me.lblFecha.Location = New System.Drawing.Point(12, 9)
+        Me.lblFecha.Name = "lblFecha"
+        Me.lblFecha.Size = New System.Drawing.Size(34, 13)
+        Me.lblFecha.TabIndex = 0
+        Me.lblFecha.Text = "fecha"
+        '
+        'lblMensaje
+        '
+        Me.lblMensaje.AutoSize = True
+        Me.lblMensaje.Location = New System.Drawing.Point(291, 40)
+        Me.lblMensaje.Name = "lblMensaje"
+        Me.lblMensaje.Size = New System.Drawing.Size(46, 13)
+        Me.lblMensaje.TabIndex = 1
+        Me.lblMensaje.Text = "mensaje"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 96)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Numero 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 135)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Numero 2"
+        '
+        'txtuno
+        '
+        Me.txtuno.Location = New System.Drawing.Point(99, 93)
+        Me.txtuno.Name = "txtuno"
+        Me.txtuno.Size = New System.Drawing.Size(100, 20)
+        Me.txtuno.TabIndex = 4
+        '
+        'txtdos
+        '
+        Me.txtdos.Location = New System.Drawing.Point(99, 135)
+        Me.txtdos.Name = "txtdos"
+        Me.txtdos.Size = New System.Drawing.Size(100, 20)
+        Me.txtdos.TabIndex = 5
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(240, 96)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(56, 52)
+        Me.btnCalcular.TabIndex = 6
+        Me.btnCalcular.Text = "+"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(269, 226)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Resultado"
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(329, 223)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(81, 20)
+        Me.txtResultado.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(422, 261)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtdos)
+        Me.Controls.Add(Me.txtuno)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblMensaje)
+        Me.Controls.Add(Me.lblFecha)
+        Me.Name = "Form1"
+        Me.Text = "Bienvenido"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblFecha As Label
+    Friend WithEvents lblMensaje As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtuno As TextBox
+    Friend WithEvents txtdos As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtResultado As TextBox
+End Class
---
+++ ProyectoSumaVariables/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim fecha As Date
+        fecha = Date.Now
+        lblFecha.Text = fecha
+
+        Dim mensaje As String
+        mensaje = "Ingrese los campos"
+        lblMensaje.Text = mensaje
+
+
+
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim n1 As Double
+        n1 = txtuno.Text
+
+        Dim n2 As Double
+        n2 = txtdos.Text
+
+        Dim res As Double
+        res = n1 + n2
+        txtResultado.Text = res
+
+
+
+
+
+
+
+
+    End Sub
+End Class
